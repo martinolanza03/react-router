a750fef1ff2a1147ec9a6ac2d431b4e24450a631
martinolanza03 <lanzamartino3@gmail.com>
2025-04-17 15:14:05 +0200

    feat: install axios, and add list of post

--- src/pages/ListPosts.jsx
+++ src/pages/ListPosts.jsx
@@ -1,6 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function ListPosts() {
+    const [posts, setPosts] = useState([]);
+
+    function listOfPosts() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                setPosts(res.data);
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    useEffect(listOfPosts, [])
+
+
+
     return <>
         <h1>Lista Post</h1>
+
+        {
+            posts.map(post => {
+                return <ul key={post.id}>
+                    <li>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </li>
+
+                </ul>
+
+            })
+        }
     </>
 }
 
